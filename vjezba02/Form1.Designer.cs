64b1589e898a0cec272a390aa3c8814072e28c39
lucija.matković <lucija.matkovi@KABINF03>
2025-10-30 19:24:48 +0100

    Add project files.

---
+++ vjezba02/Form1.Designer.cs
@@ -0,0 +1,329 @@
+﻿namespace vjezba02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIme = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbMjestoRodenja = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dtpDatumRodenja = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cbA = new System.Windows.Forms.CheckBox();
+            this.cbB = new System.Windows.Forms.CheckBox();
+            this.cbC = new System.Windows.Forms.CheckBox();
+            this.cbD = new System.Windows.Forms.CheckBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.nupIspit = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.rbMusko = new System.Windows.Forms.RadioButton();
+            this.rbZensko = new System.Windows.Forms.RadioButton();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lbFakultet = new System.Windows.Forms.ListBox();
+            this.btnPotvrdi = new System.Windows.Forms.Button();
+            this.mcKalendar = new System.Windows.Forms.MonthCalendar();
+            ((System.ComponentModel.ISupportInitialize)(this.nupIspit)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ime i prezime:";
+            // 
+            // txtIme
+            // 
+            this.txtIme.Location = new System.Drawing.Point(89, 9);
+            this.txtIme.Name = "txtIme";
+            this.txtIme.Size = new System.Drawing.Size(200, 20);
+            this.txtIme.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Mjesto rodenja:";
+            // 
+            // cbMjestoRodenja
+            // 
+            this.cbMjestoRodenja.FormattingEnabled = true;
+            this.cbMjestoRodenja.Items.AddRange(new object[] {
+            "Split",
+            "Solin",
+            "Dugopolje",
+            "Zadar",
+            "Imotski",
+            "Sinj"});
+            this.cbMjestoRodenja.Location = new System.Drawing.Point(97, 38);
+            this.cbMjestoRodenja.Name = "cbMjestoRodenja";
+            this.cbMjestoRodenja.Size = new System.Drawing.Size(202, 21);
+            this.cbMjestoRodenja.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Datum rodenja:";
+            // 
+            // dtpDatumRodenja
+            // 
+            this.dtpDatumRodenja.Location = new System.Drawing.Point(99, 74);
+            this.dtpDatumRodenja.Name = "dtpDatumRodenja";
+            this.dtpDatumRodenja.Size = new System.Drawing.Size(202, 20);
+            this.dtpDatumRodenja.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 107);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Vozačka dozvola:";
+            // 
+            // cbA
+            // 
+            this.cbA.AutoSize = true;
+            this.cbA.Location = new System.Drawing.Point(111, 107);
+            this.cbA.Name = "cbA";
+            this.cbA.Size = new System.Drawing.Size(33, 17);
+            this.cbA.TabIndex = 7;
+            this.cbA.Text = "A";
+            this.cbA.UseVisualStyleBackColor = true;
+            // 
+            // cbB
+            // 
+            this.cbB.AutoSize = true;
+            this.cbB.Location = new System.Drawing.Point(151, 107);
+            this.cbB.Name = "cbB";
+            this.cbB.Size = new System.Drawing.Size(33, 17);
+            this.cbB.TabIndex = 8;
+            this.cbB.Text = "B";
+            this.cbB.UseVisualStyleBackColor = true;
+            // 
+            // cbC
+            // 
+            this.cbC.AutoSize = true;
+            this.cbC.Location = new System.Drawing.Point(191, 107);
+            this.cbC.Name = "cbC";
+            this.cbC.Size = new System.Drawing.Size(33, 17);
+            this.cbC.TabIndex = 9;
+            this.cbC.Text = "C";
+            this.cbC.UseVisualStyleBackColor = true;
+            // 
+            // cbD
+            // 
+            this.cbD.AutoSize = true;
+            this.cbD.Location = new System.Drawing.Point(231, 107);
+            this.cbD.Name = "cbD";
+            this.cbD.Size = new System.Drawing.Size(34, 17);
+            this.cbD.TabIndex = 10;
+            this.cbD.Text = "D";
+            this.cbD.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(10, 138);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(95, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Ukupan broj ispita:";
+            // 
+            // nupIspit
+            // 
+            this.nupIspit.Location = new System.Drawing.Point(111, 136);
+            this.nupIspit.Name = "nupIspit";
+            this.nupIspit.Size = new System.Drawing.Size(68, 20);
+            this.nupIspit.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(15, 165);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(43, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Mobilni:";
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(64, 162);
+            this.maskedTextBox1.Mask = "(999) 000-00-00";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(100, 20);
+            this.maskedTextBox1.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(15, 193);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(29, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "spol:";
+            // 
+            // rbMusko
+            // 
+            this.rbMusko.AutoSize = true;
+            this.rbMusko.Location = new System.Drawing.Point(64, 191);
+            this.rbMusko.Name = "rbMusko";
+            this.rbMusko.Size = new System.Drawing.Size(56, 17);
+            this.rbMusko.TabIndex = 16;
+            this.rbMusko.TabStop = true;
+            this.rbMusko.Text = "muško";
+            this.rbMusko.UseVisualStyleBackColor = true;
+            // 
+            // rbZensko
+            // 
+            this.rbZensko.AutoSize = true;
+            this.rbZensko.Location = new System.Drawing.Point(120, 191);
+            this.rbZensko.Name = "rbZensko";
+            this.rbZensko.Size = new System.Drawing.Size(59, 17);
+            this.rbZensko.TabIndex = 17;
+            this.rbZensko.TabStop = true;
+            this.rbZensko.Text = "žensko";
+            this.rbZensko.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(15, 215);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(48, 13);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Fakultet:";
+            // 
+            // lbFakultet
+            // 
+            this.lbFakultet.FormattingEnabled = true;
+            this.lbFakultet.Items.AddRange(new object[] {
+            "FESB",
+            "PMF",
+            "Ekonomski",
+            "Pravni",
+            "Građevinski",
+            "Medicinski",
+            "DIF"});
+            this.lbFakultet.Location = new System.Drawing.Point(69, 214);
+            this.lbFakultet.Name = "lbFakultet";
+            this.lbFakultet.Size = new System.Drawing.Size(124, 69);
+            this.lbFakultet.TabIndex = 19;
+            // 
+            // btnPotvrdi
+            // 
+            this.btnPotvrdi.Location = new System.Drawing.Point(199, 215);
+            this.btnPotvrdi.Name = "btnPotvrdi";
+            this.btnPotvrdi.Size = new System.Drawing.Size(75, 69);
+            this.btnPotvrdi.TabIndex = 20;
+            this.btnPotvrdi.Text = "potvrdi";
+            this.btnPotvrdi.UseVisualStyleBackColor = true;
+            this.btnPotvrdi.Click += new System.EventHandler(this.btnPotvrdi_Click);
+            // 
+            // mcKalendar
+            // 
+            this.mcKalendar.Dock = System.Windows.Forms.DockStyle.Right;
+            this.mcKalendar.Location = new System.Drawing.Point(382, 0);
+            this.mcKalendar.Name = "mcKalendar";
+            this.mcKalendar.TabIndex = 21;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(581, 308);
+            this.Controls.Add(this.mcKalendar);
+            this.Controls.Add(this.btnPotvrdi);
+            this.Controls.Add(this.lbFakultet);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.rbZensko);
+            this.Controls.Add(this.rbMusko);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.nupIspit);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.cbD);
+            this.Controls.Add(this.cbC);
+            this.Controls.Add(this.cbB);
+            this.Controls.Add(this.cbA);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dtpDatumRodenja);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cbMjestoRodenja);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtIme);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Registracija";
+            ((System.ComponentModel.ISupportInitialize)(this.nupIspit)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtIme;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbMjestoRodenja;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dtpDatumRodenja;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox cbA;
+        private System.Windows.Forms.CheckBox cbB;
+        private System.Windows.Forms.CheckBox cbC;
+        private System.Windows.Forms.CheckBox cbD;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown nupIspit;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.RadioButton rbMusko;
+        private System.Windows.Forms.RadioButton rbZensko;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ListBox lbFakultet;
+        private System.Windows.Forms.Button btnPotvrdi;
+        private System.Windows.Forms.MonthCalendar mcKalendar;
+    }
+}
+
---
+++ vjezba02/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace vjezba02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPotvrdi_Click(object sender, EventArgs e)
+        {
+            string imePrezime, mjestoRodjenja, vozackaDozvola, mobilni, spol, faakultet;
+            DateTime datumRodjenja, danasnjiDatum;
+           
+
+        }
+    }
+}
